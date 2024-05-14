4f49241fff4e8fe546b739a65b99eb60f1eab7fd
casperrr <casper.sebastien@gmail.com>
2024-05-14 16:53:03 +0100

    working transparency

--- src/termpixtool.py
+++ src/termpixtool.py
@@ -13,19 +13,20 @@ class TermpixTool:
         self.imgPath = args.tool
 
         response = requests.get(self.imgPath)
-        self.img = Image.open(BytesIO(response.content)).convert('RGB')
+        self.img = Image.open(BytesIO(response.content)).convert('RGBA')
 
         self.img.thumbnail((70, 100), resample=Image.NEAREST)
+        # self.img.thumbnail((70, 100))
         self.img = self.img.quantize(colors=30)
-        self.img = self.img.convert('RGB')
+        self.img = self.img.convert('RGBA')
         print(self.img)
         self.createTPix()
 
         # Save the processed image
-        self.img.save('processed_image.jpg')
+        self.img.save('processed_image.png', 'PNG')
 
         # print(self.pixArray)
-        self.testPrint()
+        self.testPrint3()
         # self.testPrint2()
 
     def createTPix(self):
@@ -58,3 +59,16 @@ class TermpixTool:
                 r, g, b = pixel
                 print(f"\033[38;2;{r};{g};{b}m██", end="")
             print()
+
+    def testPrint3(self):
+        alphaThresh = 128
+        for y in range(0, len(self.pixArray), 2):
+            for x in range(len(self.pixArray[0])):
+                r1, g1, b1, a1 = self.pixArray[y][x]
+                r2, g2, b2, a2 = self.pixArray[y+1][x] if y+1 < len(self.pixArray) else (0, 0, 0, 255)
+                if a2 < alphaThresh and a1 < alphaThresh:  # If the pixel is transparent
+                    print(' ', end="")
+                else:
+                    print(f"\033[38;2;{r2};{g2};{b2}m\033[48;2;{r1};{g1};{b1}m▄", end="")
+                print("\033[0m", end="")  # Reset colors after each pixel
+            print()  # Print a newline at the end of each row
